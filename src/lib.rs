e8028a647396433d54034328876da03bf43523d3
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2025-09-08 21:28:46 +0200

    Export result structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@
 mod i2c;
 mod traits;
 
-pub use traits::TFLunaSync;
+pub use traits::{TFLunaSync, FirmwareVersion, SerialNumber, Signature, PowerMode, RangingMode};
 pub use i2c::{constants::DEFAULT_SLAVE_ADDRESS, errors, TFLuna};
